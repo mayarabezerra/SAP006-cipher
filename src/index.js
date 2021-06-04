700ca5407db4b14595fe30da69055b5eebe6b2ae
mayarabezerra <mayarab253@gmail.com>
2021-06-03 23:30:19 -0300

    atualizações da estrutura

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import cipher from './cipher.js';
+/*import cipher from './cipher.js';
 
-console.log(cipher);
+console.log(cipher);*/
+
+document.getElementById("code-button").addEventListener("click", code)
+
+function code(){
+    capturando= document.getElementById('texto').value
+    
+}
