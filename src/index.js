ec39b3f71b3da0f235d833924509c64e68834a48
mayarabezerra <mayarab253@gmail.com>
2021-06-14 22:07:29 -0300

    escape

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import cipher from './cipher.js'
 
-// falta criar variavel "const encodeButton= ..." e mostrar botão para js pela id?
 document.getElementById("code").addEventListener("click", encode)
 function encode(e){
     e.preventDefault()
-    const offset = (document.getElementById("offset").value) // value null?
+    const offset = (document.getElementById("offset").value) 
     let string = document.getElementById("texto").value 
     let encodeCipher = cipher.encode(offset, string)
-    document.getElementById("saidaTexto").value = encodeCipher //innerHTML(printar o resultado na 'saidaTexto'?/ Uso  mesma Id "texto"? (não,peguei id da saida)
+    document.getElementById("saidaTexto").value = encodeCipher 
 
 }
-// falta criar variavel "const decodeButton= ..." e mostrar botão para js pela id?
-document.getElementById("decode").addEventListener("click", decode) //evento, função
+document.getElementById("decode").addEventListener("click", decode) 
 function decode(e){
-    e.preventDefault() //adicionei para parar evento
-    const offset = (document.getElementById("offset").value) //falta indicar valor do offset "number"?
+    e.preventDefault() 
+    const offset = (document.getElementById("offset").value) 
     let string = document.getElementById("texto").value 
-    let decodeCipher = cipher.decode (offset, string) //função cipher que ta no cipher
+    let decodeCipher = cipher.decode (offset, string) 
     document.getElementById("saidaTexto").value = decodeCipher
 }
 
