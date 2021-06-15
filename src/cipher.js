b9522ffc733f3c099e6c913f39a402f277203164
mayarabezerra <mayarab253@gmail.com>
2021-06-14 22:03:14 -0300

    'Enter'

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
-const cipher = {
-  // ...
-};
 
+const cipher = {
+
+  encode: function encode(offset,string) {
+    let mensage= ""
+    let offsetNumber = parseInt(offset)
+
+    for (let i=0; i< string.length; i++){  //loop inicial 
+      let cripto= string.charCodeAt(i) //variavel que recebe e guarda o código
+      if (cripto >= 65 && cripto <= 90) { // condição para calculo/codifica
+        cripto = ((cripto - 65 + offsetNumber)% 26) + 65
+      }
+      else if (cripto >= 97 && cripto <= 122) { // condição 97 A 122 MAISCULAS ASC
+        cripto = ((cripto - 97 + offsetNumber) % 26) + 97
+      }
+      mensage += String.fromCharCode(cripto)
+    }
+    return mensage
+  },
+  decode: function decode(offset,string){
+    let resposta= ""
+    let offsetNumber= parseInt(offset) ///parseInt converter p/ string?
+  
+    for (let i=0; i<string.length; i++){ //erro de sintaxe, tinha esquecido sinal "<" e escrito length errado
+      let decode = string.charCodeAt(i) //variavel que recebe/guarda o código
+      if (decode >= 65 && decode <= 90) { // &&= rode isso e em seguida rode aquilo // condição 65=a 90=z
+        decode = 90-((90 - decode + offsetNumber)% 26);
+      }
+      else if (decode >= 97 && decode <= 122){  // condição 97 A 122 MAISCULAS
+        decode = 122-((122 -decode + offsetNumber)%26);
+      }
+      resposta += String.fromCharCode(decode)
+    } 
+    return resposta
+  
+  }
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-/*import cipher from './cipher.js';
+import cipher from './cipher.js'
 
-console.log(cipher);*/
+// falta criar variavel "const encodeButton= ..." e mostrar botão para js pela id?
+document.getElementById("code").addEventListener("click", encode)
+function encode(e){
+    e.preventDefault()
+    const offset = (document.getElementById("offset").value) // value null?
+    let string = document.getElementById("texto").value 
+    let encodeCipher = cipher.encode(offset, string)
+    document.getElementById("saidaTexto").value = encodeCipher //innerHTML(printar o resultado na 'saidaTexto'?/ Uso  mesma Id "texto"? (não,peguei id da saida)
 
-document.getElementById("code-button").addEventListener("click", code)
-
-function code(){
-    capturando= document.getElementById('texto').value
-    
 }
+// falta criar variavel "const decodeButton= ..." e mostrar botão para js pela id?
+document.getElementById("decode").addEventListener("click", decode) //evento, função
+function decode(e){
+    e.preventDefault() //adicionei para parar evento
+    const offset = (document.getElementById("offset").value) //falta indicar valor do offset "number"?
+    let string = document.getElementById("texto").value 
+    let decodeCipher = cipher.decode (offset, string) //função cipher que ta no cipher
+    document.getElementById("saidaTexto").value = decodeCipher
+}
+
+console.log("cipher");
